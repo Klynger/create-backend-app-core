05460ec03e4f18627b1d4448584c68b3afccc447
Rafael Klynger <rafafhgkifh@gmail.com>
2019-08-09 20:22:35 -0300

    Choose another port to run the app if the chosen one is occupied

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import * as detect from 'detect-port';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { INestApplication } from '@nestjs/common';
+
+const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
+
+async function run(app: INestApplication, port: number) {
+  await app.listen(port);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await detect(DEFAULT_PORT).then(port => {
+    if (port === DEFAULT_PORT) {
+      run(app, port);
+      return;
+    }
+
+    // tslint:disable-next-line:no-console
+    console.log(`Something is running on port ${
+      DEFAULT_PORT
+    }, running on port ${port}`);
+
+    process.env.PORT = port.toString();
+    run(app, port);
+  });
 }
 bootstrap();
